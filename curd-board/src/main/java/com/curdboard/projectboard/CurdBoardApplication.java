81dccc7a04ee7d43f4f9de12323e1f06346b9e7e
velSeon <seonday28@gmail.com>
2023-01-15 00:08:55 +0900

    setting gitignore and gradle

---
+++ curd-board/src/main/java/com/curdboard/projectboard/CurdBoardApplication.java
@@ -0,0 +1,13 @@
+package com.curdboard.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurdBoardApplication.class, args);
+    }
+
+}
---
+++ curd-board/src/test/java/com/curdboard/projectboard/CurdBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curdboard.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
