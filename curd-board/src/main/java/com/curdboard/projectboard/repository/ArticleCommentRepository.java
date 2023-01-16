3bfe8b29d1114abbf8dd8e68f82b61f07138ab40
velSeon <seonday28@gmail.com>
2023-01-16 19:54:52 +0900

    data rest 설정
    게시글, 댓글 json api 자동으로 restful하게끔 설정

--- curd-board/src/main/java/com/curdboard/projectboard/repository/ArticleCommentRepository.java
+++ curd-board/src/main/java/com/curdboard/projectboard/repository/ArticleCommentRepository.java
@@ -1,6 +1,8 @@
 package com.curdboard.projectboard.repository;
 import com.curdboard.projectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
--- curd-board/src/main/java/com/curdboard/projectboard/repository/ArticleRepository.java
+++ curd-board/src/main/java/com/curdboard/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.curdboard.projectboard.repository;
 
 import com.curdboard.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
