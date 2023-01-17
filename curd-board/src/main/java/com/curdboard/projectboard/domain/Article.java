615d83eb10373fc552461a352da4620407faa132
velSeon <seonday28@gmail.com>
2023-01-17 21:11:30 +0900

    필요없는 import 제거 및 클래스 abstract추가

--- curd-board/src/main/java/com/curdboard/projectboard/domain/Article.java
+++ curd-board/src/main/java/com/curdboard/projectboard/domain/Article.java
@@ -3,11 +3,6 @@ package com.curdboard.projectboard.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
