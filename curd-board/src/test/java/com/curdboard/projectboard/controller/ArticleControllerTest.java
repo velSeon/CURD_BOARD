71d30a56241e65807b529f5fa77a7c3492f8ce0b
velSeon <seonday28@gmail.com>
2023-01-17 21:15:04 +0900

    로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러 테스트에 반영
    스프링 시큐리티 자동생성된 "/login" 컨트롤러를 테스트 
    시큐리티 설정 @Import 추가

--- curd-board/src/test/java/com/curdboard/projectboard/controller/ArticleControllerTest.java
+++ curd-board/src/test/java/com/curdboard/projectboard/controller/ArticleControllerTest.java
@@ -1,10 +1,12 @@
 package com.curdboard.projectboard.controller;
 
+import com.curdboard.projectboard.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -15,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
@@ -23,6 +26,7 @@ class ArticleControllerTest {
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
+
 
 //    @Disabled("구현 중")
     @DisplayName("[view] [GET] 게시글 리스트 (게사판) 페이지 - 정상 호출")
