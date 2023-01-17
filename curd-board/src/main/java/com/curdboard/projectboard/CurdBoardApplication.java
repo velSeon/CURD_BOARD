57aee75e95bb971634351e06be2a7508c6eef043
velSeon <seonday28@gmail.com>
2023-01-17 20:32:49 +0900

    thymeleaf3 decoupled logic 설정
    thymeleaf3 decoupled logic을 사용자 application property 로 제어할 수 있게 설정 추가함

--- curd-board/src/main/java/com/curdboard/projectboard/CurdBoardApplication.java
+++ curd-board/src/main/java/com/curdboard/projectboard/CurdBoardApplication.java
@@ -2,12 +2,12 @@ package com.curdboard.projectboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class CurdBoardApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CurdBoardApplication.class, args);
     }
-
 }
