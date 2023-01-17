fb4a3fc9eb3ef7b6eaf4243d8dde5736908d3862
velSeon <seonday28@gmail.com>
2023-01-17 19:53:19 +0900

    헤더 , 풋터 분리

--- curd-board/src/test/java/com/curdboard/projectboard/controller/ArticleControllerTest.java
+++ curd-board/src/test/java/com/curdboard/projectboard/controller/ArticleControllerTest.java
@@ -46,7 +46,7 @@ class ArticleControllerTest {
         //When & Then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(MockMvcResultMatchers.view().name("articles/detail"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("article"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("articleComments"));
@@ -60,7 +60,7 @@ class ArticleControllerTest {
         //When & Then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("article/search"));
     }
     @Disabled("구현 중")
@@ -72,7 +72,7 @@ class ArticleControllerTest {
         //When & Then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("article/search-hashtag"));
     }
 
