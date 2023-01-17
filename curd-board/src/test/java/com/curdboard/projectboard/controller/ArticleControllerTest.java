3719c9ae03a456dbc88fbb2ed8157f54ea6d2a92
velSeon <seonday28@gmail.com>
2023-01-17 19:53:00 +0900

    게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메소드 테스트 통과하게 열어줌
    뷰 적절히 뼈대 잡음
    테스트에서는 media type 수정

--- curd-board/src/test/java/com/curdboard/projectboard/controller/ArticleControllerTest.java
+++ curd-board/src/test/java/com/curdboard/projectboard/controller/ArticleControllerTest.java
@@ -24,7 +24,7 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
+//    @Disabled("구현 중")
     @DisplayName("[view] [GET] 게시글 리스트 (게사판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -33,7 +33,7 @@ class ArticleControllerTest {
         //When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(MockMvcResultMatchers.view().name("articles/index"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("articles"));
     }
